57628c2f09f9b5b52d0dd6074592d2a9a8b76231
Dominique Ducommun <dducommun@infinit8.io>
2018-01-19 09:59:59 -0800

    Soldier model and demo fixture

--- resistanceproject/resistanceapp/admin.py
+++ resistanceproject/resistanceapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Soldier
+
 # Register your models here.
+admin.site.register(Soldier)
--- resistanceproject/resistanceapp/models.py
+++ resistanceproject/resistanceapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Soldier(models.Model):
+    name=models.CharField(max_length=200)
+    age=models.IntegerField()
+    strength=models.IntegerField()
+    description=models.TextField()
+
+    def __str__(self):
+        return self.name
