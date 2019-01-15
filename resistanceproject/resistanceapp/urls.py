30f95943fd9913c7d1845300c9d2da6afe39568a
Dominique Ducommun <dducommun@infinit8.io>
2019-01-15 11:41:46 +0100

    Route for demo API urls

--- resistanceproject/resistanceapp/urls.py
+++ resistanceproject/resistanceapp/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 
 from . import views
+
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('soldiers', views.SoldierViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -12,4 +18,5 @@ urlpatterns = [
     path('dashboard/soldier/<pk>/update', views.SoldierUpdateView.as_view(), name='soldier-update'),
     path('dashboard/soldier/<pk>/delete', views.SoldierDeleteView.as_view(), name='soldier-delete'),
     path('dashboard/soldier/deadonthefield', views.SoldierDeadOnTheField.as_view(), name='soldier-dead'),
+    path('api/v1/', include(router.urls))
 ]
