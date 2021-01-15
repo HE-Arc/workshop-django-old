f55364e899012a59b10e59ed686e967a69f33bc0
Rajinth Ravindranathan <rajinth.ravindranathan@he-arc.ch>
2021-01-15 11:28:44 +0100

    finished workshop

--- resistanceproject/resistanceapp/admin.py
+++ resistanceproject/resistanceapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
-# from .models import Soldier, Category
+from .models import Soldier, Category
 
 # Register your models here.
 # TODO-1-4 - Register Soldier model in django admin
+admin.site.register(Soldier)
 # TODO-5-2 - Register Category model in admin
+admin.site.register(Category)
