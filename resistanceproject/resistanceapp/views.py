3a99f1fb31bbd7ab66e82f790130474b753ee765
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2022-01-09 10:38:53 +0100

    Update resistanceapp views indentation

--- resistanceproject/resistanceapp/views.py
+++ resistanceproject/resistanceapp/views.py
@@ -16,20 +16,20 @@ def index(request):
 class DashboardView(generic.TemplateView):
     template_name = "resistanceapp/dashboard.html"
 
-    # TODO-6-0 - Write queries for categories, 3 best soldiers by strength, count alive soldiers, count dead soldiers
+# TODO-6-0 - Write queries for categories, 3 best soldiers by strength, count alive soldiers, count dead soldiers
 
-    # TODO-3-0 Create the soldier list view with django class based views
+# TODO-3-0 Create the soldier list view with django class based views
 
-    # TODO-3-2 Create the soldier detail view with django class based views
+# TODO-3-2 Create the soldier detail view with django class based views
 
-    # TODO-4-0 - Create the soldier create view with django class based views
+# TODO-4-0 - Create the soldier create view with django class based views
 
-    # TODO-4-2 - Create the soldier update view with django class based views
+# TODO-4-2 - Create the soldier update view with django class based views
 
-    # TODO-4-4 - Create the soldier delete view with django class based views
+# TODO-4-4 - Create the soldier delete view with django class based views
 
-    # TODO-5-6 - Add category field for soldier to create and update views
+# TODO-5-6 - Add category field for soldier to create and update views
 
-    # TODO-8-0 - Write a view to set a soldier dead :( (sad)
+# TODO-8-0 - Write a view to set a soldier dead :( (sad)
 
-    # TODO-ADV-1-2 - Write views using your serializers https://www.django-rest-framework.org/tutorial/quickstart/#views
+# TODO-ADV-1-2 - Write views using your serializers https://www.django-rest-framework.org/tutorial/quickstart/#views
