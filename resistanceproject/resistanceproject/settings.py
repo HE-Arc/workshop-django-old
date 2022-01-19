72b86a1acbd4a3ddbb4de2bf55d22e022df16ba7
Lenny Boegli <lenny.boegli@he-arc.ch>
2022-01-19 09:50:40 +0100

    TODO-0-0 done

--- resistanceproject/resistanceproject/settings.py
+++ resistanceproject/resistanceproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'resistanceapp',
     # TODO-0-0 Install django-bootstrap4 via pip and add package to INSTALLED_APPS (bootstrap4)
+    'bootstrap4',
     # TODO-ADV-1-0 Install djangorestframework via pip and add package to INSTALLED_APPS (rest_framework)
 ]
 
